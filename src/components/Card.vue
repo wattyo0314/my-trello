fc94c31f440147704ad3ea89d2371077fb2cbbf4
wattyo0314 <wantyan-wattyo.0314@outlook.jp>
2020-10-01 21:02:11 +0900

    作成したカードを表示

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <button class="closeButton" @click="removeCardFromList">×</button>
+    <div class="body">
+      {{ body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    body: {
+      style: String,
+      required: true,
+    },
+    listIndex: {
+      style: Number,
+      required: true,
+    },
+    cardIndex: {
+      style: Number,
+      required: true,
+    },
+  },
+  methods: {
+    removeCardFromList: function() {
+      if (confirm("本当にこのカードを削除しますか？")) {
+        this.$store.dispatch("removeCardFromList", {
+          cardIndex: this.cardIndex,
+          listIndex: this.listIndex,
+        });
+      }
+    },
+  },
+};
+</script>
